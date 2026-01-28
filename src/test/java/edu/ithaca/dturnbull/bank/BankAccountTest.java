1a3afee4ed548c4c0fc5451f0a0d7832dce61f3b
baneetpukhrambam <bpukhrambam@ithaca.edu>
2026-01-28 10:49:44 -0500

    added more tests for isEmailValid

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -3,7 +3,6 @@ package edu.ithaca.dturnbull.bank;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-
 class BankAccountTest {
 
     @Test
@@ -14,7 +13,7 @@ class BankAccountTest {
     }
 
     @Test
-    void withdrawTest() throws InsufficientFundsException{
+    void withdrawTest() throws InsufficientFundsException {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
         bankAccount.withdraw(100);
 
@@ -23,11 +22,30 @@ class BankAccountTest {
     }
 
     @Test
-    void isEmailValidTest(){
-        assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
-        assertFalse( BankAccount.isEmailValid(""));         // empty string
+    void isEmailValidTest() {
+        assertTrue(BankAccount.isEmailValid("a@b.com"));
+        assertTrue(BankAccount.isEmailValid("abc_def@mail.com"));
+        assertTrue(BankAccount.isEmailValid("abc.def@mail-archive.com"));
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.com"));
+        assertTrue(BankAccount.isEmailValid("abc-d@mail.com"));
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.com"));
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.cc"));
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.org"));
 
-        
+        assertFalse(BankAccount.isEmailValid("")); // empty string
+        assertFalse(BankAccount.isEmailValid("ab.com")); // missing @ symbol
+        assertFalse(BankAccount.isEmailValid("abc-@mail.com")); // terminates with special character before @
+        assertFalse(BankAccount.isEmailValid("abc..def@mail.com")); // consecutive special characters
+        assertFalse(BankAccount.isEmailValid(".abc@mail.com")); // starts with special character
+        assertFalse(BankAccount.isEmailValid("abc#def@mail.com")); // invalid special character
+        assertFalse(BankAccount.isEmailValid("abc.def@mail.c")); // domain suffix only has one character
+        assertFalse(BankAccount.isEmailValid("abc.def@mail#archive.com")); // special character in domain name
+        assertFalse(BankAccount.isEmailValid("abc.def@mail")); // missing domain suffix
+        assertFalse(BankAccount.isEmailValid("abc.def@mail..com")); // consecutive special characters in domain name
+        assertFalse(BankAccount.isEmailValid("abc@def@mail.com")); // multiple @ symbols
+        assertFalse(BankAccount.isEmailValid("@mail.com")); // missing local part
+        assertFalse(BankAccount.isEmailValid("abc@")); // missing domain part
+
     }
 
     @Test
@@ -36,8 +54,8 @@ class BankAccountTest {
 
         assertEquals("a@b.com", bankAccount.getEmail());
         assertEquals(200, bankAccount.getBalance(), 0.001);
-        //check for exception thrown correctly
-        assertThrows(IllegalArgumentException.class, ()-> new BankAccount("", 100));
+        // check for exception thrown correctly
+        assertThrows(IllegalArgumentException.class, () -> new BankAccount("", 100));
     }
 
 }
