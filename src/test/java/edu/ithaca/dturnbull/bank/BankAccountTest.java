518717f5873dfdc06f4b2d93aa08d744bf5f6681
baneetpukhrambam <bpukhrambam@ithaca.edu>
2026-01-30 10:49:42 -0500

    Added more tests for isEmailValid

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -54,12 +54,60 @@ class BankAccountTest {
         assertFalse(BankAccount.isEmailValid("abc@def@mail.com")); // multiple @ symbols (border case)
 
         // Invalid Special Character Locations
-        assertFalse(BankAccount.isEmailValid("abc.def@mail..com")); // consecutive special characters in domain name 
+        assertFalse(BankAccount.isEmailValid("abc.def@mail..com")); // consecutive special characters in domain name
         assertFalse(BankAccount.isEmailValid(".abc@mail.com")); // starts with special character (border case)
-        assertFalse(BankAccount.isEmailValid("abc-@mail.com")); // terminates with special character before @ (border case)
+        assertFalse(BankAccount.isEmailValid("abc-@mail.com")); // terminates with special character before @ (border
+                                                                // case)
 
         // Improper Length
         assertFalse(BankAccount.isEmailValid("abc.def@mail.c")); // domain suffix only has one character (border case)
+
+        // Task 01-03: Additional Test Cases
+        // Blank String
+        assertFalse(BankAccount.isEmailValid("")); // string is blank (border case)
+        assertTrue(BankAccount.isEmailValid("j@l.or")); // minimum valid email (border case)
+        assertTrue(BankAccount.isEmailValid("def@jgh.com")); // valid string length (middle case)
+
+        // Prefix is blank
+        assertFalse(BankAccount.isEmailValid(" @google.com")); // blank prefix (border)
+        assertTrue(BankAccount.isEmailValid("b@google.com")); // minimum prefix length (border)
+        assertTrue(BankAccount.isEmailValid("abc@hotmail.com")); // prefix length is three (middle case)
+
+        // Domain is blank
+        assertFalse(BankAccount.isEmailValid(" baneet@")); // blank domain (border)
+        assertTrue(BankAccount.isEmailValid("b@g.co")); // minimum domain length (border)
+        assertTrue(BankAccount.isEmailValid("abc@yahoo.com")); // domain length more than three (middle case)
+
+        // Domain tag length
+        assertFalse(BankAccount.isEmailValid("abc@def.g")); // domain tag length one (border)
+        assertTrue(BankAccount.isEmailValid("abc@def.go")); // domain tag length two (border)
+        assertTrue(BankAccount.isEmailValid("yrf@outlook.com")); // doman tag length three (middle case)
+
+        // invalid special characters
+        assertFalse(BankAccount.isEmailValid("abd)def@google.com")); // one invalid special character in prefix (border)
+        assertTrue(BankAccount.isEmailValid("abddef@google.com")); // no invalid special character in prefix (border)
+        assertFalse(BankAccount.isEmailValid("abd())def@google.com")); // two invalid special character in prefix
+                                                                       // (middle)
+
+        // special character locations
+        assertFalse(BankAccount.isEmailValid("-baneet@google.com")); // prefix starts with special character (border)
+        assertFalse(BankAccount.isEmailValid("baneet-@google.com")); // prefix ends with special character (border)
+        assertTrue(BankAccount.isEmailValid("ba-neet@google.com")); // prefix has special character in the middle
+                                                                    // (middle case)
+
+        // consecutive special characters
+        assertTrue(BankAccount.isEmailValid("ba_neet@ithaca.edu")); // prefix has no consecutive special characters
+                                                                    // (border)
+        assertFalse(BankAccount.isEmailValid("ban__eet@ithaca.edu")); // prefix has two consecutive special characters
+                                                                      // (border)
+        assertFalse(BankAccount.isEmailValid("ba...neet@ithaca.edu")); // prefix has three consecutive special
+                                                                       // characters (middle)
+
+        // number of @ symbols
+        assertTrue(BankAccount.isEmailValid("bpukhrambam@ithaca.edu")); // valid email with single @ symbol (border)
+        assertFalse(BankAccount.isEmailValid("bpukh@ram@bam@ithaca")); // invalid email with multiple @ symbols (middle)
+        assertFalse(BankAccount.isEmailValid("bpukhrambamithaca.edu")); // invalid email with no @ symbol (border)
+        assertFalse(BankAccount.isEmailValid("bpukh@rambam@ithaca.edu")); // invalid email with two @ symbols (border)
     }
 
     @Test
